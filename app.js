5d29fac07692e67c448206b3c1cd99a6f0373a37
07-mvieira <caio.vieira2807@gmail.com>
2025-04-22 12:17:54 -0300

    ...............

---
+++ app.js
@@ -0,0 +1,12 @@
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+drawSceneOne();
+
+function drawSceneOne(){
+    sceneOne = new Image()
+    sceneOne.src = 'assets/backgrounds/screen-1.png';
+    sceneOne.onload = function(){
+        ctx.drawImage(sceneOne, 0, 0);
+    }
+}
