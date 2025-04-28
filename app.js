29cb4d60a64ec9dde5cb8b48fd563874cb1a609a
07-mvieira <caio.vieira2807@gmail.com>
2025-04-28 09:41:12 -0300

    a

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ let ctx = canvas.getContext("2d");
 drawSceneOne();
 
 function drawSceneOne(){
-    sceneOne = new Image()
+    let sceneOne = new Image()
     sceneOne.src = 'assets/backgrounds/screen-1.png';
     sceneOne.onload = function(){
-        ctx.drawImage(sceneOne, 0, 0);
+        ctx.drawImage(sceneOne, 0, 0, 1758, 128);
     }
 }
