6f02fa45c3139c8fbfb972fb8b99afb186267f25
07-mvieira <caio.vieira2807@gmail.com>
2025-04-29 12:14:02 -0300

    the actual beginning

--- app.js
+++ app.js
@@ -1,28 +1,7 @@
-async function init(){
-kaboom({
-    global: true,  // Define que as variáveis e funções serão globais
-    canvas: document.querySelector("canvas"),  // Define o canvas onde o jogo será renderizado
-    height: 527.41,
-    width: 658.89,
-    scale: 1,  // Escala da tela (não há escalamento)
-});
-    let bgImage = await loadSprite("background", "assets\backgrounds\screen-1.png");
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
 
-    let background = add([
-        sprite("background"),
-        // Make the background centered on the screen
-        pos(width() / 2, height() / 2),
-        origin("center"),
-        // Allow the background to be scaled
-        scale(1),
-        // Keep the background position fixed even when the camera moves
-        fixed()
-      ]);
-      // Scale the background to cover the screen
-      background.scaleTo(Math.max(
-        width() / bgImage.tex.width,
-        height() / bgImage.tex.height
-      ));
-};
+const screenHeight = 527.41
+const screenWidth = 658.89
 
-init();
+let kirby = {}
