6412527e0694a4c7aa76e20f27806b4802e389ee
07-mvieira <caio.vieira2807@gmail.com>
2025-05-05 11:22:47 -0300

    i dont know

--- app.js
+++ app.js
@@ -4,20 +4,28 @@ NÚMEROS CHAVE
 escala original = 4.120390625
 escala x4 = 1.03009765625
 
+escala original = 4
+escala x4 = 1
+
 */
 
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
+let keysDown = {}
 
+
+const titleScreen = document.getElementById("title-screen");
 const screenOne = document.getElementById("screen-1");
 const screenTwo = document.getElementById("screen-2");
 const screenThree = document.getElementById("screen-3");
 const screenFour = document.getElementById("screen-4");
 const screenFive = document.getElementById("screen-5");
 
+const tile = document.getElementById("tile");
+
 let currentScreen = screenOne;
 
-let kirbyAnimations = [
+/*let kirbyAnimations = [
     [idleRight], [idleLeft],
     [walkRight], [walkLeft],
     [jumpRight], [jumpLeft],
@@ -33,34 +41,67 @@ let kirbyAnimations = [
     [swallowRight], [swallowLeft],
     [spitRight], [spitLeft],
     [warpStar], [die],
-]
+]*/
 
 let kirbyAnimationIndex = 4;
-let kirbyfacingLeft = false;
+let kirbyFacingLeft = false;
+let kirbyFalling = true;
+let kirbyPeakJumping = false;
+let kirbyHovering = false
+let kirbyFull = false
+let kirbyHurt = false
+let hp = 6
+if (hp > 6){
+    hp = 6
+}
+let score = 0
 
+const canvasHeight = 512;
+const canvasWidth = 640;
+const tileSize = 64;
+let distanceFromFloor = 3*tileSize;
 
-const canvasHeight = 527.41;
-const canvasWidth = 658.89;
-const tileSize = 65.92625;
+let scrollX = 0;
 
-let kirby = {
-    spriteWidth: 65.92625,
-    spriteHeight: 65.92625,
-};
+let gameRunning = false;
+let gameOver = false;
 
 let screenOneTiling = [
     [
         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,99,],
         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,99,],
         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,99,],
-        [0,0,k,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,99,],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,99,],
         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,2,0,0,0,0,0,0,1,1,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,99,],
         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,5,1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,2,0,0,0,0,0,1,1,1,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,99,],
         [1,1,1,1,1,1,1,5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,5,1,1,1,1,5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,99,],
         [1,1,1,1,1,1,1,5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,5,1,1,1,1,1,5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,99,],
     ],
 ];
-    context.drawImage(screenOne, 0, 0, canvasWidth, canvasHeight)
+    
+let currentScreenTiling = screenOneTiling;
+
+let kirby = {
+    spriteWidth: 64,
+    spriteHeight: 64,
+};
+
+let drawTile = function(x, y, tileIndex) {
+    let sx = (tileIndex*64) - (128*Math.floor(tileIndex/128)*64);
+    let sy = Math.floor(tileIndex/128)*64;
+    context.drawImage(tile, sx, sy, 64, 64, x, y, 64, 64);
+}
+var drawLevelTiles = function(level, hTiles, vTiles, scrollX) {
+    for (var i=0; i<hTiles; i++) {
+        for (var j=0; j<vTiles; j++) {
+            if (level[j][i] == 1) {
+                drawTile(64*i-scrollX, j*64, 18+64*3);
+            }
+        }
+    }
+}
+
+context.drawImage(screenOne, 0, 0, 640, 512, 0, 0, 300, 150)
 
 let FPS = 60;
 let interval = 1 / FPS;
