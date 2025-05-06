15da2ed0c8994cdd23594edd5d98190d66d95a97
07-mvieira <caio.vieira2807@gmail.com>
2025-05-06 11:59:05 -0300

    actual progress
    - added collision (but no way to test it)
    - merged screen visuals and tiling collisions (for screen one) into single objects
    - building blocks to be developed once kirby actually gets added
    
    which is probably the next logical step, making kirby's spritesheets and actually programming his collision and gravity

--- app.js
+++ app.js
@@ -1,29 +1,39 @@
-/* 
-NÚMEROS CHAVE
-
-escala original = 4.120390625
-escala x4 = 1.03009765625
-
-escala original = 4
-escala x4 = 1
-
-*/
-
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 let keysDown = {}
 
 
-const titleScreen = document.getElementById("title-screen");
-const screenOne = document.getElementById("screen-1");
-const screenTwo = document.getElementById("screen-2");
-const screenThree = document.getElementById("screen-3");
-const screenFour = document.getElementById("screen-4");
-const screenFive = document.getElementById("screen-5");
+const titleScreen = {
+    vis: document.getElementById("title-screen")
+}
+
+const screenOne = {
+    vis: document.getElementById("screen-1"),
+    tiling: [
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,2,0,0,0,0,0,0,1,1,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,5,1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,2,0,0,0,0,0,1,1,1,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,],
+            [1,1,1,1,1,1,1,5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,5,1,1,1,1,5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,],
+            [1,1,1,1,1,1,1,5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,5,1,1,1,1,1,5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,],
+        ],
+}
+const screenTwo = {
+    vis: document.getElementById("screen-2"),
+}
+const screenThree = {
+    vis: document.getElementById("screen-3"),
+}
+const screenFour = {
+    vis: document.getElementById("screen-4"),
+}
+const screenFive = {
+    vis: document.getElementById("screen-2"),
+}
 
 const tile = document.getElementById("tile");
-
-let currentScreen = screenOne;
 
 /*let kirbyAnimations = [
     [idleRight], [idleLeft],
@@ -51,13 +61,10 @@ let kirbyHovering = false
 let kirbyFull = false
 let kirbyHurt = false
 let hp = 6
-if (hp > 6){
-    hp = 6
-}
 let score = 0
 
-const canvasHeight = 512;
 const canvasWidth = 640;
+const canvasHeight = 512;
 const tileSize = 64;
 let distanceFromFloor = 3*tileSize;
 
@@ -66,42 +73,99 @@ let scrollX = 0;
 let gameRunning = false;
 let gameOver = false;
 
-let screenOneTiling = [
-    [
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,99,],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,99,],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,99,],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,99,],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,2,0,0,0,0,0,0,1,1,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,99,],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,5,1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,2,0,0,0,0,0,1,1,1,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,99,],
-        [1,1,1,1,1,1,1,5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,5,1,1,1,1,5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,99,],
-        [1,1,1,1,1,1,1,5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,5,1,1,1,1,1,5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,99,],
-    ],
-];
-    
-let currentScreenTiling = screenOneTiling;
-
 let kirby = {
-    spriteWidth: 64,
-    spriteHeight: 64,
+    entityWidth: 64,
+    entityHeight: 64,
+    levelX: 192,
+    levelY: 512-256-64, // altura da tela-4 tiles acima da linha inferior-altura (?????)
+    renderX: 64,
+    renderY: 512-256-64,
+    velocityY: 0
 };
 
-let drawTile = function(x, y, tileIndex) {
-    let sx = (tileIndex*64) - (128*Math.floor(tileIndex/128)*64);
-    let sy = Math.floor(tileIndex/128)*64;
-    context.drawImage(tile, sx, sy, 64, 64, x, y, 64, 64);
+let drawTile = function(x, y) {
+    context.drawImage(tile, x, y, 64, 64); // desenha tiles invisíveis de 64x64
 }
-var drawLevelTiles = function(level, hTiles, vTiles, scrollX) {
-    for (var i=0; i<hTiles; i++) {
-        for (var j=0; j<vTiles; j++) {
-            if (level[j][i] == 1) {
-                drawTile(64*i-scrollX, j*64, 18+64*3);
+
+let drawScreenTiles = function(screen, hTiles, vTiles, scrollX) {
+    for (let i=0; i<hTiles; i++) { // definição para os tiles horizontais
+        for (let j=0; j<vTiles; j++) { // definição para os tiles verticais
+            if (screen[j][i] == 1 || screen[j][i] == 5) {
+                drawTile(64*i-scrollX, j*64);
             }
         }
     }
 }
 
-context.drawImage(screenOne, 0, 0, 640, 512, 0, 0, 300, 150)
+var checkentityTileCollision = function(entity, tileX, tileY, collisions) {
+    if ((entity.levelX + entity.entityWidth >= tileX &&
+        entity.levelX <= tileX + TILE_SIZE) && 
+        (entity.levelY + entity.entityHeight >= tileY &&
+            entity.levelY <= tileY + TILE_SIZE)) {
+                let intersect = {
+                    x: Math.max(entity.levelX,tileX),
+                    y: Math.max(entity.levelY,tileY),
+                }
+                intersect.width = Math.min(entity.levelX + entity.entityWidth, 
+                    tileX + TILE_SIZE) - intersect.x;
+                intersect.height = Math.min(entity.levelY + entity.entityHeight, 
+                    tileY + TILE_SIZE) - intersect.y;
+                if (entity.levelX+entity.entityWidth/2 > tileX+TILE_SIZE/2 && 
+                    intersect.height > intersect.width) collisions.left = true;
+                if (entity.levelX+entity.entityWidth/2 < tileX+TILE_SIZE/2 && 
+                    intersect.height > intersect.width) collisions.right = true;
+                if (entity.levelY+entity.entityHeight/2 < tileY+TILE_SIZE/2 && 
+                    intersect.width > intersect.height && intersect.width > 4) {
+                    collisions.top = true;
+                    collisions.topY = tileY;
+                }
+                if (entity.levelY+entity.entityHeight/2 > tileY+TILE_SIZE/2 && 
+                    intersect.width > intersect.height && intersect.width > 4) {
+                        collisions.bottom = true;
+                }
+        }
+}
+
+context.drawImage(screenOne.vis, 0, 0, 640, 512, 0, 0, 300, 150)
+
+let resetGame = function() {
+    kirby = {
+        entityWidth: 64,
+        entityHeight: 64,
+        levelX: 192,
+        levelY: 512-256-64,
+        renderX: 64,
+        renderY: 512-256-64,
+        velocityY: 0
+    }
+    
+    currentScreen = JSON.parse(JSON.stringify(screenOne.tiling[0]));
+}
+
+let gameLoop = function(interval) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (kirby.levelX + kirby.entityWidth/2 >= canvasWidth/2 && 
+        scrollX+canvasWidth < currentScreen[0].length*tileSize ||
+        (kirby.levelX + canvasWidth/2 + kirby.entityWidth/2 <= 
+        currentScreen[0].length*canvasWidth && scrollX > 0)) {
+        scrollX = kirby.levelX - canvasWidth/2 + kirby.entityWidth/2;
+    }
+
+    if (kirby.levelY + kirby.entityHeight/2 >= canvasHeight/2 && 
+        scrollX+canvasHeight < currentScreen[0].length*tileSize ||
+        (kirby.levelX + canvasHeight/2 + kirby.entityWidth/2 <= 
+        currentScreen[0].length*canvasHeight && scrollX > 0)) {
+        scrollX = kirby.levelX - canvasHeight/2 + kirby.entityWidth/2;
+    }
+
+    hero.renderX = hero.levelX - scrollX;
+    hero.renderY = hero.levelY;
+
+    drawScreenTiles(currentScreen, currentScreen[0].length, currentScreen.length, scrollX);
+}
+
+currentLevel = JSON.parse(JSON.stringify(screenOne.tiling[0]));
 
 let FPS = 60;
 let interval = 1 / FPS;
